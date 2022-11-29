c48f7ec9da18f107d9e39f6cba0536de7c6f717d
Guilherme Ramos <guilhermeramos999@gmail.com>
2022-11-28 21:31:56 -0300

    release

--- view/login/login-form.php
+++ view/login/login-form.php
@@ -5,43 +5,42 @@
     <div class="row d-flex justify-content-center align-items-center h-100">
       <div class="col-md-9 col-lg-6 col-xl-5">
         <h1 style="font-size: 24px; font-style: italic;" class="text-primary text-center"> Domine seu trabalho, organize sua vida, lembre-se de tudo, enfrente cada projeto com suas anotações e com praticidade no <span style="font-weight: bold; color: black;">Study Note<span></h1>
-        <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
-          class="img-fluid" alt="Sample image">
+        <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp" class="img-fluid" alt="Sample image">
       </div>
       <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
-        
+
         <form action="/authlogin" method="POST">
-          <a class="form-outline mb-3 d-flex justify-content-center align-items-center"href="">
+          <a class="form-outline mb-3 d-flex justify-content-center align-items-center" href="">
             <img src="https://i.postimg.cc/4yH5qS4L/logo.jpg" alt="logo">
           </a>
 
           <!-- Email input -->
           <div class="form-outline mb-3">
-            <input type="email"name="email" id="form3Example3" class="form-control form-control-lg" placeholder="Digite o seu E-mail" required/>
+            <input type="email" name="email" id="form3Example3" class="form-control form-control-lg" placeholder="Digite o seu E-mail" required />
             <label for="email" class="form-label"></label>
           </div>
 
           <!-- Password input -->
           <div class="form-outline mb-3">
-            <input type="password" id="form3Example4" name="password"class="form-control form-control-lg" placeholder="Digite a sua Senha" required/>
+            <input type="password" id="form3Example4" name="password" class="form-control form-control-lg" placeholder="Digite a sua Senha" required />
             <label class="form-label" for="password"></label>
           </div>
 
           <div class="text-center text-lg-start mt-3 pt-2">
 
             <button class="btn btn-primary btn-lg" style="padding-left: 2.5rem; padding-right: 2.5rem;">Entrar</button>
-            <p class="small fw-bold mt-2 pt-1 mb-0">Ainda não tem conta? <a href="/register"
-                class="link-blue">Cadastre-se</a></p> 
+            <p class="small fw-bold mt-2 pt-1 mb-0">Ainda não tem conta? <a href="/register" class="link-blue">Cadastre-se</a></p>
           </div>
 
         </form>
+        <?php if (isset($_SESSION['message'])) : ?>
+          <p class="alert alert-<?= $_SESSION['message_type'] ?>"><?= $_SESSION['message'] ?></p>
+        <?php endif ?>
       </div>
-      
+
     </div>
   </div>
-  
+
 </section>
 
 <?php require __DIR__ . '/../html-footer.php';
-
-
--- view/study/form.php
+++ view/study/form.php
@@ -1,35 +1,34 @@
 <?php require __DIR__ . '/../html-header.php' ?>
 
-<section class=""
-  style="margin-top: 100px">
+<section class="" style="margin-top: 100px">
   <div class="mask d-flex align-items-center h-100 gradient-custom-3">
     <div class="container h-100 ">
       <div class="row d-flex justify-content-center align-items-center h-100">
         <div class="col-12 col-md-9 col-lg-7 col-xl-6">
           <div class="card" style="border-radius: 15px;">
             <div class="card-body p-5">
-              <h2 class="text-uppercase text-center mb-5">Cadastre um Tópico</h2>
+              <h2 class="text-uppercase text-center mb-5"><?= !isset($item) ? 'Cadastre' : 'Edite' ?> um Tópico</h2>
 
               <form action="/saveitem<?= is_null($item) ? '' : '?id=' . $item->id ?>" style="width: 30em;" class="ms-3 align-items-center " method="POST">
 
                 <div class="form-outline mb-4">
-                    <label for="name" class="form-label">Título</label>
-                    <input type="text" class="form-control" name="name" value="<?= is_null($item) ? '' : $item->name ?>" required><br>
+                  <label for="name" class="form-label">Título</label>
+                  <input type="text" class="form-control" name="name" value="<?= is_null($item) ? '' : $item->name ?>" required><br>
                 </div>
 
                 <div class="form-outline mb-4">
-                    <label for="video" class="form-label">Url</label>
-                    <input type="url" name="video" class="form-control" value=" <?= is_null($item) ? '' : $item->video ?>"><br>
+                  <label for="video" class="form-label">Url</label>
+                  <input type="url" name="video" class="form-control" value=" <?= is_null($item) ? '' : $item->video ?>"><br>
                 </div>
 
                 <div class="form-outline mb-4">
-                    <label for="description" class="form-label">Descrição</label>
-                    <textarea name="description" class="form-control" required><?= is_null($item) ? '' : $item->description ?></textarea><br><br>
+                  <label for="description" class="form-label">Descrição</label>
+                  <textarea name="description" class="form-control" required><?= is_null($item) ? '' : $item->description ?></textarea><br><br>
                 </div>
 
                 <div class="d-flex justify-content-center">
-                    <button class="btn btn-primary">Salvar</button>
-                    <a href="/list" class="btn btn-secondary ms-3">Voltar</a>
+                  <button class="btn btn-primary">Salvar</button>
+                  <a href="/list" class="btn btn-secondary ms-3">Voltar</a>
                 </div>
 
                 <p class="text-center text-black mt-5 mb-0"><b>OBS:</b> Ao salvar uma URL de um vídeo, o mesmo será reproduzido.</p>
